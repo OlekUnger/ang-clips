f5edc5ceb0d77b4e27dd956fe32ce29dcf2663c8
o.unger <ounger@inbox.ru>
2024-10-15 19:34:09 +0300

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"], // where to search tailwind classes
+  theme: {
+    extend: {},
+    fontFamily: {
+      roboto: ["Roboto"] // font-roboto
+    }
+  },
+  plugins: [],
+}
+
